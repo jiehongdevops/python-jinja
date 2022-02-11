9748a4961bb9620927323154a572b15085bdbba6
jhoppo <jack.hong@oppo-aed.tw>
2022-02-11 21:14:52 +0800

    update

--- practice/practice/practice.py
+++ practice/practice/practice.py
@@ -1,1 +1,5 @@
+from jinja2 import Environment
 
+template = open("email.j2", "r").read()
+templated = Environment().from_string(template).render(noobAccount='nora.yang.a00814',Password='Today@0214',Email='nora.yang.a00814@oppo-aed.tw',printer='345621')
+open("nora.yang.a00814.html", "w").write(templated)
